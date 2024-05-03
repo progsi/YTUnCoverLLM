40798a2077d0c0e5d9f0cea662288869de80a4ac
progsi <simon@hachmeier.de>
2024-05-03 17:06:43 +0000

    running baseline

--- baseline/prepare.sh
+++ baseline/prepare.sh
@@ -1,6 +1,11 @@
 cd music-ner-eacl2023
-python3 music-ner/datasets/create_seen_rare_ds.py --data_dir data/dataset1/ --th_seen=1 --th_rare_unseen=0
-python3 music-ner/datasets/create_seen_rare_ds.py --data_dir data/dataset2/ --th_seen=1 --th_rare_unseen=0
-python3 music-ner/datasets/create_seen_rare_ds.py --data_dir data/dataset3/ --th_seen=1 --th_rare_unseen=0
-python3 music-ner/datasets/create_seen_rare_ds.py --data_dir data/dataset4/ --th_seen=1 --th_rare_unseen=0
+python music-ner/datasets/stats.py --data_dir=data/dataset1
+python music-ner/datasets/stats.py --data_dir=data/dataset2
+python music-ner/datasets/stats.py --data_dir=data/dataset3
+python music-ner/datasets/stats.py --data_dir=data/dataset4
+
+python music-ner/datasets/create_seen_rare_ds.py --data_dir data/dataset1/ --th_seen=1 --th_rare_unseen=0
+python music-ner/datasets/create_seen_rare_ds.py --data_dir data/dataset2/ --th_seen=1 --th_rare_unseen=0
+python music-ner/datasets/create_seen_rare_ds.py --data_dir data/dataset3/ --th_seen=1 --th_rare_unseen=0
+python music-ner/datasets/create_seen_rare_ds.py --data_dir data/dataset4/ --th_seen=1 --th_rare_unseen=0
 mkdir output
