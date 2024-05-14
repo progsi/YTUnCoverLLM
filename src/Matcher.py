422ceda3553a3f89caad7516e21897c2e0954114
progsi <simon@hachmeier.de>
2024-05-14 13:12:35 +0000

    pairwise matching

--- src/Matcher.py
+++ src/Matcher.py
@@ -3,6 +3,7 @@ import torch
 import pandas as pd
 from typing import List, Callable
 from abc import abstractmethod, ABC
+import numpy as np
 
 
 class Matcher(ABC):
@@ -22,7 +23,8 @@ class Matcher(ABC):
         y = self._match_square(left_data, right_data)
         return pd.DataFrame(y, index=df[left_attrs].index, columns=df[right_attrs].index)
     
-    def match_pairwise(self, df: pd.DataFrame, left_attrs: List[str], right_attrs: List[str], level: str):
+    def match_pairwise(self, df: pd.DataFrame, left_attrs: List[str], right_attrs: List[str], 
+                       level: str):
         """Match all pairs as in df.
         Args:
             df (pd.DataFrame): pandas Dataframe including the metadata
@@ -44,7 +46,8 @@ class Matcher(ABC):
             df[col_name] = self._match_pairwise(left_data, right_data)
 
         elif level == "attr":
-            combis = pd.MultiIndex.from_product([left_attrs, right_attrs],  names=['left_attrs', 'right_attrs'])
+            combis = pd.MultiIndex.from_product([left_attrs, right_attrs],  
+                                                names=['left_attrs', 'right_attrs'])
 
             for combi in combis:
                 
@@ -71,28 +74,36 @@ class Matcher(ABC):
     
     @staticmethod
     def __get_concat_col_name(left_attrs: List[str], right_attrs: List[str]):
-        """Generate column name for the col with the pairwise similarity of concatenated columns (eg. entity-level).
+        """Generate column name for the col with the pairwise similarity of concatenated columns 
+        (eg. entity-level).
         Args:
             left_attrs (List[str]): attributes on the left side
             right_attrs (List[str]): attributes on the right side
         """
         return ('+'.join([attr for attr in left_attrs]), '+'.join([attr for attr in right_attrs]))    
-
-    @abstractmethod
-    def _func(self, func_str):
-        pass
     
     @abstractmethod
     def _match_square(left_data: List[str], right_data: List[str]):
+        """Match all N^2 pairs of items in left and right data.
+        Args:
+            left_data (List[str]): left strings 
+            right_data (List[str]): right strings
+        """
         pass
 
     @abstractmethod
     def _match_pairwise(left_data: List[str], right_data: List[str]):
+        """Match all pairs of strings in left and right data. Only strings at the 
+        matching indices are matched.
+        Args:
+            left_data (List[str]): left strings 
+            right_data (List[str]): right strings
+        """
         pass
 
 class FuzzyMatcher(Matcher):
     def __init__(self, func_str: str, workers: int = 64) -> None:
-        self.func = self._func(func_str)
+        self.func = self._func_from_str(func_str)
         self.workers = workers
 
     def _match_pairwise(self, left_data: List[str], right_data: List[str]):
@@ -101,19 +112,31 @@ class FuzzyMatcher(Matcher):
     def _match_square(self, left_data: List[str], right_data: List[str]):
         return process.cdist(left_data, right_data, scorer=self.func, workers=self.workers)
     
-    def _func(self, func_str: str):
+    def _func_from_str(self, func_str: str):
+        """Get callable from name string.
+        Args:
+            func_str (str): Name of the function
+        Returns:
+            Callable[[str, str], float]: calleble string matching function
+        """
         return eval(func_str)
     
 class SimpleMatcher(Matcher):
-    def __init__(self, func: str) -> None:
-        self.func = self._func(func)
+    """Implements a simple Matcher.
+    Args:
+        func (Callable[[str, str], float]): matching function.
+    """
+    def __init__(self, func: Callable[[str, str], float]) -> None:
+        self.func = func
 
     def _match_pairwise(self, left_data: List[str], right_data: List[str]):
-
-        return pd.DataFrame(zip(left_data, right_data), columns=["left", "right"]).apply(lambda x: self.func(x.left, x.right), axis=1).values
+        return pd.DataFrame(zip(left_data, right_data), columns=["left", "right"]).apply(
+            lambda x: self.func(x.left, x.right), axis=1).values
     
     def _match_square(self, left_data: List[str], right_data: List[str]):
-        raise NotImplementedError(f"Square matching not implemented for {self.func}")
-    
-    def _func(self, func: Callable[[str, str], float]):
-        return func
+        n1, n2 = len(left_data), len(right_data)
+        m = np.zeros((n1, n2))
+        for i in range(n1):
+            for j in range(n2):
+                m[i][j] = self.func(left_data[i], right_data[j])
+        return m
