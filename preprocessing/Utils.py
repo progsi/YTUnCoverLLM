a5dd53d439f505a4af66bc57c701c2df89105db0
progsi <simon@hachmeier.de>
2024-05-24 10:30:05 +0000

    minor

--- preprocessing/Utils.py
+++ preprocessing/Utils.py
@@ -2,6 +2,27 @@ import pandas as pd
 import unicodedata
 import re
 
+# order important! Due to overlaps between title and artist strings.
+SONG_ATTRS = ["title", "title_perf", "title_work", "performer", "performer_perf", "performer_work"]
+CLASS_ATTRS = ["split", "set_id", "ver_id", "yt_id"]
+YT_ATTRS = ["video_title", "channel_name", "description"]
+
+# mapping to classes from the baseline paper (coarse)
+BASELINE_NAMES = {
+    "title": "WoA", 
+    "performer": "Artist", 
+    "title_perf": "WoA", 
+    "performer_perf": "Artist", 
+    "title_work": "WoA", 
+    "performer_work": "Artist",
+    "ambiguous": "Artist_or_WoA"
+}
+
+# for label names
+B_PREFIX = "B-"
+I_PREFIX = "I-"
+O_LABEL = "O"
+
 def replace_linebreaks_tabs(series: pd.Series) -> pd.Series:
     return series.str.replace("\n", " ").str.replace("\t", " ").str.replace("\r", " ")
 
@@ -32,28 +53,4 @@ def remove_bracket_only(s: str) -> str:
 
 def unicode_normalize(s: str) -> str:
     return unicodedata.normalize('NFKD', s).encode('ascii', 'ignore').decode('utf-8')
-
-
-# order important! Due to overlaps between title and artist strings.
-SONG_ATTRS = ["title", "title_perf", "title_work", "performer", "performer_perf", "performer_work"]
-CLASS_ATTRS = ["split", "set_id", "ver_id", "yt_id"]
-YT_ATTRS = ["video_title", "channel_name", "description"]
-
-# mapping to classes from the baseline paper (coarse)
-BASELINE_NAMES = {
-    "title": "WoA", 
-    "performer": "Artist", 
-    "title_perf": "WoA", 
-    "performer_perf": "Artist", 
-    "title_work": "WoA", 
-    "performer_work": "Artist",
-    "ambiguous": "Artist_or_WoA"
-}
-
-# for label names
-B_PREFIX = "B-"
-I_PREFIX = "I-"
-O_LABEL = "O"
-
-
 
