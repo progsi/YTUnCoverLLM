7310a4cb678951a49c4bb048ad407fdb00ab3ce2
progsi <simon@hachmeier.de>
2024-05-28 13:08:45 +0000

    modified paths

--- baseline/prepare_csi_data.sh
+++ baseline/prepare_csi_data.sh
@@ -1,16 +1,16 @@
 cd ..
-datacos_path="/data/csi_datasets/datacos_yt.parquet"
-shs100k2_path="/data/csi_datasets/shs100k2_yt.parquet"
+datacos_path="data/raw/datacos_yt.parquet"
+shs100k2_path="data/raw/shs100k2_yt.parquet"
 
 # shs100k2
 echo "Creating shs100k2 NER biotag dataset"
-python preprocessing/1_preprocess_dataset.py -i "$shs100k2_path" -o data/shs100k2_processed.parquet --split
-python preprocessing/2_make_biotag_dataset.py -i data/shs100k2_processed.parquet -o data/shs100k2_biotag.parquet --all --baseline_names
-python preprocessing/3_format_biotag_dataset.py -i data/shs100k2_biotag.parquet -o baseline/music-ner-eacl2023/data/shs100k2/ --minimum_ents 2
+python preprocessing/1_preprocess_dataset.py -i "$shs100k2_path" -o data/intermediate/shs100k2_processed.parquet --split
+python preprocessing/2_make_biotag_dataset.py -i data/intermediate/shs100k2_processed.parquet -o data/intermediate/shs100k2_biotag.parquet --all --baseline_names
+python preprocessing/3_format_biotag_dataset.py -i data/intermediate/shs100k2_biotag.parquet -o baseline/music-ner-eacl2023/data/shs100k2/ --minimum_ents 2
 
 # datacos
 echo "Creating datacos NER biotag dataset"
-python preprocessing/1_preprocess_dataset.py -i "$datacos_path" -o data/datacos_processed.parquet --split
-python preprocessing/2_make_biotag_dataset.py -i data/datacos_processed.parquet -o data/datacos_biotag.parquet --all --baseline_names
-python preprocessing/3_format_biotag_dataset.py -i data/datacos_biotag.parquet -o baseline/music-ner-eacl2023/data/datacos/ --minimum_ents 2
+python preprocessing/1_preprocess_dataset.py -i "$datacos_path" -o data/intermediate/datacos_processed.parquet --split
+python preprocessing/2_make_biotag_dataset.py -i data/intermediate/datacos_processed.parquet -o data/intermediate/datacos_biotag.parquet --all --baseline_names
+python preprocessing/3_format_biotag_dataset.py -i data/intermediate/datacos_biotag.parquet -o baseline/music-ner-eacl2023/data/datacos/ --minimum_ents 2
 cp baseline/music-ner-eacl2023/data/shs100k2/train.bio  baseline/music-ner-eacl2023/data/datacos/
