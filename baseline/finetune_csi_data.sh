c3a0bd8313582fd8e09a177341c14750405cef99
progsi <simon@hachmeier.de>
2024-06-11 09:30:53 +0000

    minor fix

--- baseline/finetune_csi_data.sh
+++ baseline/finetune_csi_data.sh
@@ -33,11 +33,11 @@ NUM_EPOCHS=3
 REINIT_LAYERS=1
 SEED=1
 
-DATA_DIR="data/shs100k2/"
+DATA_DIR="data/shs100k2/complete/"
 for MODEL in bert-large-uncased roberta-large microsoft/mpnet-base
 do
 	BASE_NAME=$(basename ${MODEL})
-	OUTPUT_DIR="output/shs100k2/"$BASE_NAME
+	OUTPUT_DIR="output/shs100k2/complete/"$BASE_NAME
 	python music-ner/src/fine-tune.py --dataset_name music-ner/datasets --model_name_or_path $MODEL --output_dir $OUTPUT_DIR --num_train_epochs $NUM_EPOCHS --per_device_train_batch_size $BATCH_SIZE --seed $SEED --do_train --do_predict --overwrite_output_dir --reinit_layers $REINIT_LAYERS --return_entity_level_metrics --dataset_path=$DATA_DIR --save_strategy epoch
 done
 
